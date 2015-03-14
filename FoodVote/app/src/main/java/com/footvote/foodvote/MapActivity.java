c47b3cae049e25296a8514cf3a084cfe83849d93
Shikib <mehrishikib@gmail.com>
2015-03-13 21:59:51 -0700

    added google maps view

--- FoodVote/app/src/main/java/com/footvote/foodvote/MapActivity.java
+++ FoodVote/app/src/main/java/com/footvote/foodvote/MapActivity.java
@@ -1,26 +1,24 @@
 package com.footvote.foodvote;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MapActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
     }
 
@@ -37,9 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void openMap(View v) {
-        startActivity(new Intent(this, MapActivity.class));
     }
 }
