63ff7bede2f7eb556f93b7d4c16ab7509b70e897
Shikib <mehrishikib@gmail.com>
2015-03-14 13:38:28 -0700

    implemented auto completion for google maps search bar. produces a weird error atm.

---
+++ FoodVote/app/src/main/java/com/footvote/foodvote/PlacesMapActivity.java
@@ -0,0 +1,23 @@
+package com.footvote.foodvote;
+
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by NWHacks on 14/03/2015.
+ */
+public class PlacesMapActivity extends MapActivity {
+    double latitude;
+    double longitude;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_places);
+
+        Intent i = getIntent();
+
+        // Users current geo location
+        String user_latitude = i.getStringExtra("user_latitude");
+        String user_longitude = i.getStringExtra("user_longitude");
+    }
+}
---
+++ FoodVote/app/src/main/java/com/footvote/foodvote/google/AlertDialogManager.java
@@ -0,0 +1,45 @@
+package com.footvote.foodvote.google;
+
+/**
+ * Created by NWHacks on 14/03/2015.
+ */
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.footvote.foodvote.R;
+
+public class AlertDialogManager {
+    /**
+     * Function to display simple Alert Dialog
+     * @param context - application context
+     * @param title - alert dialog title
+     * @param message - alert message
+     * @param status - success/failure (used to set icon)
+     *               - pass null if you don't want icon
+     * */
+    public void showAlertDialog(Context context, String title, String message,
+                                Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+
+        if(status != null)
+            // Setting alert dialog icon
+            alertDialog.setIcon((status) ? R.drawable.common_signin_btn_icon_disabled_focus_light : R.drawable.common_signin_btn_icon_disabled_focus_dark);
+
+        // Setting OK Button
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+}
