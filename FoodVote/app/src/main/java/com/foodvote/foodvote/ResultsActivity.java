5936a26b9df289e36d1ba79ec05042510b4d3748
mparkms <mikeparkms@gmail.com>
2015-03-15 07:53:20 -0700

    results activity

--- FoodVote/app/src/main/java/com/foodvote/foodvote/ResultsActivity.java
+++ FoodVote/app/src/main/java/com/foodvote/foodvote/ResultsActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 
 public class ResultsActivity extends ActionBarActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
